606fbd0ee37a777939b39026d28aa535e7729fa4
Haziq Kamel <haziqkamel@outlook.com>
2024-07-01 18:21:07 +0800

    chore: run build_runner for todo model

--- packages/todos_api/lib/src/todos_api.dart
+++ packages/todos_api/lib/src/todos_api.dart
@@ -15,6 +15,12 @@ abstract class TodosApi {
   /// If a [todo] with the same id already exists, it will be updated.
   Future<void> saveTodo(Todo todo);
 
+  /// Deletes the `todo` with the given id.
+  ///
+  /// If no `todo` with the given id exists, a [TodoNotFoundException] error is
+  /// thrown.
+  Future<void> deleteTodo(String id);
+
   /// Deletes all completed todos.
   ///
   /// Returns the number of deleted todos.
